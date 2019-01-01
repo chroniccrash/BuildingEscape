9a5f346e35e12a25654c331352d0c87d59624c31
Akelo <chronic.crash@gmail.com>
2019-01-01 00:19:09 -0500

    added logging to PositionReport class

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -8,6 +9,7 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+
 
 	// ...
 }
@@ -18,6 +20,11 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
+	AActor* currentOwner;
+	currentOwner = GetOwner();
+
+	FString itemObject = currentOwner->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("PositionReport reporting for duty on %s"), *itemObject);
 	// ...
 	
 }
